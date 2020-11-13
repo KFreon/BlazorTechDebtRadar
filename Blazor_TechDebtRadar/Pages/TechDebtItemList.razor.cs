6f6078420ded8f512ea9b0a9258ceb97ae812fa8
Kael Larkin <kael.larkin@purple.telstra.com>
2020-11-13 14:04:17 +1000

    Testing codebehind, shared styles, etc

---
+++ Blazor_TechDebtRadar/Pages/TechDebtItemList.razor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Blazor_TechDebtRadar.Services;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Blazor_TechDebtRadar.Pages
+{
+    public partial class TechDebtItemList
+    {
+        private string? SearchText { get; set; }
+        private IReadOnlyCollection<InMemoryStorageItem> Items = new List<InMemoryStorageItem>();
+
+        private System.Timers.Timer searchInputTimer = new System.Timers.Timer()
+        {
+            Interval = 0.2 * 1000
+        };
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Items = storage.GetItems();
+            searchInputTimer.Elapsed += (_, _) =>
+            {
+                Console.WriteLine($"Searching for {SearchText}");
+                searchInputTimer.Stop();
+                Console.Out.Flush();
+
+                PerformSearch();
+            };
+        }
+
+        private void PerformSearch()
+        {
+            var allItems = storage.GetItems();
+            if (SearchText is null)
+            {
+                Items = allItems;
+                return;
+            }
+
+            Items = allItems
+                .Where(item => item.Title.Contains(SearchText, StringComparison.OrdinalIgnoreCase)
+                    || item.Description.Contains(SearchText, StringComparison.OrdinalIgnoreCase)
+                ).ToImmutableArray();
+
+            InvokeAsync(() => StateHasChanged());
+        }
+
+        public void OnSearchTextChanged(KeyboardEventArgs arg)
+        {
+            searchInputTimer.Stop();
+            searchInputTimer.Start();
+        }
+
+        public void AddNewItem()
+        {
+            navigator.NavigateTo("/new-item");
+        }
+    }
+}
---
+++ Blazor_TechDebtRadar/Pages/TechDebtListItemItem.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Blazor_TechDebtRadar.Pages
+{
+    public partial class TechDebtListItemItem
+    {
+        [Parameter] public string Title { get; set; }
+        [Parameter] public string Description { get; set; }
+        [Parameter] public DateTimeOffset EstimatedDateOfIncident { get; set; }
+        [Parameter] public DateTimeOffset CreatedOn { get; set; }
+        [Parameter] public string CreatedBy { get; set; }
+    }
+}
