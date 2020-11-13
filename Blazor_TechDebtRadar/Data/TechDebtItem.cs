e6b9e190ded18e4d32e61c730c3b7b2e36cc005d
Kael Larkin <kael.larkin@purple.telstra.com>
2020-11-13 11:46:38 +1000

    Add project files.

---
+++ Blazor_TechDebtRadar/Data/TechDebtItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blazor_TechDebtRadar.Data
+{
+    public class TechDebtItem
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 10, ErrorMessage = "Title should be between 10-50")]
+        public string Title { get; set; } = "";
+
+        [Required]
+        [StringLength(200, MinimumLength = 10, ErrorMessage = "Description should be between 10-200")]
+        public string Description { get; set; } = "";
+
+        [Required]
+        public DateTimeOffset EstimatedDateOfIncident { get; set; } = DateTimeOffset.Now;
+    }
+}
---
+++ Blazor_TechDebtRadar/Services/InMemoryStorageService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using Blazor_TechDebtRadar.Data;
+
+namespace Blazor_TechDebtRadar.Services
+{
+    public class InMemoryStorageItem : TechDebtItem
+    {
+        public DateTimeOffset CreatedOn { get; set; }
+        public string CreatedBy { get; set; }
+
+        public InMemoryStorageItem(TechDebtItem item)
+        {
+            Title = item.Title;
+            Description = item.Description;
+            EstimatedDateOfIncident = item.EstimatedDateOfIncident;
+            CreatedOn = DateTimeOffset.Now;
+            CreatedBy = "MEEEE";
+        }
+    }
+
+    public class InMemoryStorageService
+    {
+        private readonly List<InMemoryStorageItem> _items = new List<InMemoryStorageItem>
+        {
+            new InMemoryStorageItem(new TechDebtItem
+            {
+                Title="Google Maps API deprecation",
+                Description="Maps are getting deleted entirely cos Google.",
+                EstimatedDateOfIncident=DateTimeOffset.Now.AddDays(2)
+            }),
+            new InMemoryStorageItem(new TechDebtItem
+            {
+                Title="Apple is garbage",
+                Description="Stop making excuses, you know they are.",
+                EstimatedDateOfIncident=DateTimeOffset.Now.AddDays(20)
+            }),
+        };
+
+        public IReadOnlyCollection<InMemoryStorageItem> GetItems() => _items.ToImmutableArray();
+
+        public void Add(InMemoryStorageItem item) => _items.Add(item);
+    }
+}
